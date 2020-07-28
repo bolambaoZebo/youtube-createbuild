d8df85b071e21f26974172bfb73a46bad849583e
DNA-Z <dna-z@DNA-Redner-2.local>
2020-07-29 06:49:49 +0800

    added constants.swift and constantsTempalate.swift

---
+++ youtube-createbuild/Constant.swift
@@ -0,0 +1,9 @@
+//
+//  Constanst.swift
+//  youtube-createbuild
+//
+//  Created by DNA-Z on 7/29/20.
+//  Copyright © 2020 DNA-Z. All rights reserved.
+//
+
+import Foundation
---
+++ youtube-createbuild/ConstantsTemplate.swift
@@ -0,0 +1,9 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-createbuild
+//
+//  Created by DNA-Z on 7/29/20.
+//  Copyright © 2020 DNA-Z. All rights reserved.
+//
+
+import Foundation
